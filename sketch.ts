d9f1d512055a5756f583f54c3caf14c821a74991
unknown <awsssayah@gmail.com>
2022-06-06 22:01:58 +0100

    changed main to sketch and introduced working parse function

---
+++ sketch.ts
@@ -0,0 +1,49 @@
+function token(type,value){
+	this.type=type
+	this.value=value
+}
+
+enum SymbolType{
+	num=0,
+	ope
+}
+
+function Eval(){
+	let inp=document.getElementById("Input"),
+		out=document.getElementById("Output")
+	console.log('Evaluating ...')
+	console.log(Parse("10+5"))
+}
+
+
+function Parse(expr){
+	let out=[]
+	let curnum=0
+	for(let i=0;i<expr.length){
+		if(expr.charAt(i).isNaN()){
+			//not a number => operation
+			//push the previous number then the operation
+			out.push(new token(SymbolType.num,curnum))
+			switch(expr.charAt(i)){
+				case "+":
+					out.push(new token(SymbolType.ope,"+"))
+					break
+				case "*":
+					out.push(new token(SymbolType.ope,"*"))
+					break
+			}
+			curnum=0
+		}else{
+			curnum=curnum*10+Number(expr.charAt(i))
+		}
+	}
+	return out
+}
+
+function ShuntingYard(expr){
+	let stk1=[],
+		stk2=[]
+
+}
+
+
